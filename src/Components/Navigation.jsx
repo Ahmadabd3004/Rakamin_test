7739dfcc7109599badc4b2988ba9cce88da8bcb8
Ahmadabd3004 <ahmadazis3004@gmail.com>
2023-01-05 08:10:07 +0700

    Animate the menu component

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -1,29 +1,54 @@
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useTransition, animated } from '@react-spring/web'
 
 function Navigation() {
     const [showMenu, setShowMenu] = useState(false)
-    let menu;
+    const maskTransitions = useTransition(showMenu, {
+        from: { position: "absolute", opacity: 0 },
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+    })
+
+    const menuTransitions = useTransition(showMenu, {
+        from: { opacity: 0, transform: "translateX(-100%)" },
+        enter: { opacity: 1, transform: "translateX(0%)" },
+        leave: { opacity: 0, transform: "translateX(-100%)" },
+    })
     let menuMask;
     if (showMenu) {
-        menu = 
-        <div className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow">
-            The menu
-        </div>
-
         menuMask =
-        <div
-            className="bg-black opacity-50 fixed top-0 left-0 w-full h-full z-50"
-            onClick={() => setShowMenu(false)}
-        ></div>
+            <div
+                className="bg-black opacity-50 fixed top-0 left-0 w-full h-full z-50"
+                onClick={() => setShowMenu(false)}
+            ></div>
     }
     return (
         <>
             <nav>
                 <FontAwesomeIcon icon={faBars} onClick={() => setShowMenu(!showMenu)} />
-                {menuMask}
-                {menu}
+                {
+                    maskTransitions((style, item) => (
+                        item &&
+                        <animated.div style={style}>{menuMask}</animated.div>
+                    ))
+                }
+
+                {
+                    menuTransitions((style, item) => (
+                        item &&
+                        <animated.div style={style} className="fixed bg-white top-0 left-0 w-3/5 h-full z-50 shadow p-3">
+                            <span className="font-bold">
+                                Menu
+                            </span>
+                            <ul>
+                                <li>HomePage</li>
+                            </ul>
+                        </animated.div>
+                    ))
+                }
+                {/* {menuMask} */}
             </nav>
         </>
     )
